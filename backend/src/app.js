2f2e956e6b8e13a38b6971d06e2d77020f367024
Manthan Bhatt <bhattmanthan8@gmail.com>
2026-07-18 20:54:34 +0530

    razor pay integration

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use("/public", express.static(path.join(__dirname, "../public")));
 
 // Root endpoint
 app.get("/", (req, res) => {
